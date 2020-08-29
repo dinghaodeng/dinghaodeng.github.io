ac8e266d87f781fac71ba5b21dd495e6fe042029
dinghaodeng <dinghaoden1@gmail.com>
2020-08-28 17:48:38 -0700

    Updates

---
+++ precache-manifest.8b336be8cb7c84c651b1cb780630ff6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cd08051c691b1a4cbaf05b19fd02a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "304d3f44391d24e0625e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac718f0f523bdb879964",
+    "url": "/static/js/2.5c7f7a78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c7f7a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304d3f44391d24e0625e",
+    "url": "/static/js/main.30a67239.chunk.js"
+  },
+  {
+    "revision": "1f4dc50e31f7998525dc",
+    "url": "/static/js/runtime-main.d8b32011.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
